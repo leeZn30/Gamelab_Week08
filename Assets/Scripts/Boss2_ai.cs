825e18518b9861690885504c4e677187eaced021
Wooseok Cho <crasy4368@gmail.com>
2024-09-04 23:20:07 +0900

    feat: Boss2 Pattern first
    ~2nd pattern

--- Assets/Scripts/Boss2_ai.cs
+++ Assets/Scripts/Boss2_ai.cs
@@ -1,9 +1,137 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss2_ai : MonoBehaviour
 {
+    private int currentPhase;
+
+    private PatternConditionState currentPatternConditionState;
+
+    [SerializeField] private PatternProbabilityScriptableObj[] phase1_PatternProbabilities;
+    [SerializeField] private PatternProbabilityScriptableObj[] phase2_PatternProbabilities;
+
+    private void PlayRandomPattern(int phase)
+    {
+        PatternProbabilityScriptableObj _patternPool;
+        if (phase == 0)
+            _patternPool = phase1_PatternProbabilities[((int)currentPatternConditionState)];
+        else
+        {
+            _patternPool = phase2_PatternProbabilities[((int)currentPatternConditionState)];
+        }
+
+        int _randomNum = UnityEngine.Random.Range(0, 100);
+        int _addNum = 0;
+        for(int i = 0; i < phase1_PatternProbabilities.Length; i++)
+        {
+            if (_randomNum < _patternPool.Probability[i])
+            {
+                PlayPattern(_patternPool.PatternName[i]);
+                break;
+            }
+
+            _addNum += _patternPool.Probability[i];
+        }
+    }
+
+    private void PlayIntro()
+    {
+
+    }
+
+    private void PlayPattern(string patternName)
+    {
+        PatternState state = PatternState.idle;
+        Enum.TryParse(patternName, out state);
+
+        switch (state) { 
+            case PatternState.idle:
+                PlayIdle();
+                break;
+            case PatternState.pattern1:
+                PlayPattern_1();
+                break;
+            case PatternState.pattern2:
+                PlayPattern_2();
+                break;
+            case PatternState.pattern2_1:
+                PlayPattern_2_1();
+                break;
+            case PatternState.pattern3:
+                PlayPattern_3();
+                break;
+            case PatternState.pattern4:
+                PlayPattern_4();
+                break;
+            case PatternState.pattern5:
+                PlayPattern_5();
+                break;
+            case PatternState.pattern6:
+                PlayPattern_6();
+                break;
+            case PatternState.walk:
+                PlayWalk();
+                break;
+            case PatternState.turn:
+                Turn();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void PlayPattern_1()
+    {
+
+    }
+
+    private void PlayPattern_2()
+    {
+
+    }
+
+    private void PlayPattern_2_1()
+    {
+
+    }
+
+    private void PlayPattern_3()
+    {
+
+    }
+
+    private void PlayPattern_4()
+    {
+
+    }
+
+    private void PlayPattern_5()
+    {
+
+    }
+
+    private void PlayPattern_6()
+    {
+
+    }
+
+    private void PlayWalk()
+    {
+        UnityEngine.Random.Range(1, 0);
+    }
+
+    private void PlayIdle()
+    {
+
+    }
+
+    private void Turn()
+    {
+
+    }
+
     private enum PatternState
     {
         idle,
@@ -21,13 +149,11 @@ public class Boss2_ai : MonoBehaviour
         pattern5,
         pattern6,
     }
-
-
 }
 
 public enum PatternConditionState
 {
-    Idle,
+    Normal,
     RightBehind,
     Close,
     Far,
