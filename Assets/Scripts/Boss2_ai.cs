b46f217cb01b812bc04e740a0ad0e5d9ca1445bf
Wooseok Cho <crasy4368@gmail.com>
2024-09-04 18:43:17 +0900

    feat: boss2 ai first

--- Assets/Scripts/Boss2_ai.cs
+++ Assets/Scripts/Boss2_ai.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Boss2_ai : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum PatternState
     {
-        
+        idle,
+        walk,
+        turn,
+        pattern1,
+        pattern1_1,
+        pattern1_2,
+        pattern2,
+        pattern2_1,
+        pattern3,
+        pattern3_1,
+        pattern3_2,
+        pattern4,
+        pattern5,
+        pattern6,
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+}
+
+public enum PatternConditionState
+{
+    Idle,
+    RightBehind,
+    Close,
+    Far,
 }
