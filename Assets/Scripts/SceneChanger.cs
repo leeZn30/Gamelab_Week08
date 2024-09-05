8faf1e0d3df58c93aa745176a7b8423d2e9a3dcb
leeZn30 <55116981+leeZn30@users.noreply.github.com>
2024-09-05 14:01:47 +0900

    메인씬 제작

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void GoGameScene()
+    {
+        SceneManager.LoadScene("01_Game");
+    }
+}
