e090455b08e0da8a0978dc40fdc195cd4bb5f855
KuBonJae <qhswo9@gmail.com>
2024-09-07 23:27:52 +0900

    [fix] 닷지 버그 수정 外
    - velocity로 닷지 방향 정하던 이상한 코드 수정
    - 차지 중 캔슬 가능 ( 공격 나간 후로는 캔슬 불가능 )
    - 공격 애니메이션과 idle 애니매이션이 부드럽게 이어지게 해 후딜 끝나는 시점을 알기 쉽게 함
    - 선입력 수정 ( 닷지 & 공격에 대한 버퍼 수정 )

--- Assets/Scripts/Player/Action/Dodge.cs
+++ Assets/Scripts/Player/Action/Dodge.cs
@@ -81,6 +81,7 @@ public class Dodge : MonoBehaviour
         _animator.SetBool("dodging", true);
         _animator.SetBool("walkToDodge", true);
         _animator.SetBool("isMoving", false);
+        _animator.SetBool("doAttack", false);
         float positiveDirection = _move.lastLookDirection; // (_body.velocity.x == 0 ? _move.lastLookDirection : Mathf.Sign(_body.velocity.x));
         transform.localScale = new Vector3(positiveDirection > 0f ? -_move.playerScale : _move.playerScale, _move.playerScale, _move.playerScale);
         Vector2 destination = new Vector2((positiveDirection > 0f ? _body.position.x + DodgeDistance : _body.position.x - DodgeDistance), _body.position.y);
@@ -121,15 +122,17 @@ public class Dodge : MonoBehaviour
             _playerController.playerContext.CanPlayerDodge();
             pressButton = context.ReadValueAsButton();
 
+            if(!buttonBuffer)
+                _dodgeBuffer = 0f;
             buttonBuffer = true;
-            _dodgeBuffer = 0f;
         }
         else
         {
             if(context.performed)
             {
+                if (!buttonBuffer)
+                    _dodgeBuffer = 0f;
                 buttonBuffer = true;
-                _dodgeBuffer = 0f;
             }
         }
     }
